96456c3ea7b59478c6cdf212edfa24e240e536d8
Bo van den Berg <Bo03@planet.nl>
2021-02-09 14:28:33 +0100

    updated PlayerMovement

--- Project PaperBoii/PaperBoii/Assets/Bo-Folder/C# Scripts/Movement/MovementPlayerGrid.cs
+++ Project PaperBoii/PaperBoii/Assets/Bo-Folder/C# Scripts/Movement/MovementPlayerGrid.cs
@@ -6,25 +6,49 @@ public class MovementPlayerGrid : MonoBehaviour
 {
     [Header("Positioning")]
     [SerializeField] private int LaneCount;
+    [SerializeField] private float LaneSpacing;
+
+    [Header("Dev Statistics")]
+    [SerializeField] private List<Vector2> lanePositions = new List<Vector2>();
+    [SerializeField] private int playerLaneIndex;
 
     private KeyBinds currentKeyBinds;
 
     private void Awake()
     {
         EventManager.current.updateKeyBinds += UpdateKeyBinds;
+
+        for (int i = 0; i < LaneCount; i++)
+        {
+            lanePositions.Add(new Vector2(-LaneSpacing * (float)LaneCount / 2 + i + LaneSpacing / 2, 0));
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKey(currentKeyBinds.moveLeft))
+        if (Input.GetKeyDown(currentKeyBinds.moveLeft))
         {
-            transform.position += new Vector3(0, 0, -1) * Time.deltaTime;
+            transform.position += new Vector3(0, 0, GetNewPlayerLaneIndex(-1));
         }
 
-        if (Input.GetKey(currentKeyBinds.moveRight))
+        if (Input.GetKeyDown(currentKeyBinds.moveRight))
         {
-            transform.position += new Vector3(0, 0, 1) * Time.deltaTime;
+            transform.position += new Vector3(0, 0, GetNewPlayerLaneIndex(1));
         }
+    }
+
+    private int GetNewPlayerLaneIndex(int iDifference)
+    {
+        int result = playerLaneIndex;
+
+        result += iDifference;
+
+        while (result >= lanePositions.Count)
+        {
+            result -= lanePositions.Count;
+        }
+
+        return result;
     }
 
     private void UpdateKeyBinds(KeyBinds iKeyBinds)
