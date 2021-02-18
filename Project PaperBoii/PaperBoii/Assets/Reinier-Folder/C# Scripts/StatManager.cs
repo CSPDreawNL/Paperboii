3c814faa5d4f9d34f98b34d9b86b7e5369c9715f
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-02-18 13:37:36 +0100

    Update to canvas
    Canvas updated
    Test script for money removed

--- Project PaperBoii/PaperBoii/Assets/Reinier-Folder/C# Scripts/StatManager.cs
+++ Project PaperBoii/PaperBoii/Assets/Reinier-Folder/C# Scripts/StatManager.cs
@@ -4,12 +4,8 @@ using TMPro;
 public class StatManager : MonoBehaviour
 {
     [SerializeField] GameObject deathPanel;
-    [SerializeField] TextMeshProUGUI moneyText;
-    [SerializeField] TextMeshProUGUI moneyTextPanel;
     [SerializeField] TextMeshProUGUI timeText;
     [SerializeField] TextMeshProUGUI timeTextPanel;
-    [SerializeField] TextMeshProUGUI papersText;
-    [SerializeField] TextMeshProUGUI papersTextPanel;
     [SerializeField] float moneyAmount = 0;
     [SerializeField] int paperAmount = 0;
     [SerializeField] float playTime = 0;
@@ -30,11 +26,6 @@ public class StatManager : MonoBehaviour
 
     private void Start()
     {
-        moneyText.text = "$" + moneyAmount;
-        moneyTextPanel.text = "$" + moneyAmount;
-        papersText.text = "Papers: " + paperAmount;
-        papersTextPanel.text = "Papers: " + paperAmount;
-
         EventManager.current.onPlayerDie += Death;
         Debug.Log("Instancie");
     }
@@ -57,32 +48,5 @@ public class StatManager : MonoBehaviour
         string playTimeString = string.Format("{00:00}", playTime);
         timeText.text = "Playtime: " + minutes + ":" + playTimeString;
         timeTextPanel.text = "Playtime: " + minutes + ":" + playTimeString;
-    }
-
-    public void AddMoney(float amount)
-    {
-        moneyAmount += amount;
-        moneyText.text = "$" + moneyAmount;
-        moneyTextPanel.text = "$" + moneyAmount;
-    }
-
-    public void LoseMoney(float amount)
-    {
-        moneyAmount -= amount;
-        moneyText.text = "$" + moneyAmount;
-        moneyTextPanel.text = "$" + moneyAmount;
-    }
-
-    public void AddPaper(int amount)
-    {
-        paperAmount += amount;
-        papersText.text = "Papers: " + paperAmount;
-        papersTextPanel.text = "Papers: " + paperAmount;
-    }
-    public void LosePaper(int amount)
-    {
-        paperAmount -= amount;
-        papersText.text = "Papers: " + paperAmount;
-        papersTextPanel.text = "Papers: " + paperAmount;
     }
 }
